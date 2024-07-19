046d8fcaf321fe458aee2b279694205985922175
Awet Mohammed <awetjemal@gmail.com>
2024-07-19 15:17:19 +0000

    All project tests are passed 28 out of 28.

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -2,6 +2,9 @@ package com.example.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -76,6 +79,64 @@ but will not contain an accountId.
         }else{
             return ResponseEntity.status(400).body(null);
         }
+    }
+
+    /*As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages.
+    - The response body should contain a JSON representation of a list containing all messages retrieved from the database. 
+    It is expected for the list to simply be empty if there are no messages. The response status should always be 200, which is the default. */
+    @GetMapping("messages")
+    public ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> allMessages = messageService.getAllMessages();
+        return ResponseEntity.status(200).body(allMessages);
+    }
+
+    /*As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/messages/{messageId}.
+    - The response body should contain a JSON representation of the message identified by the messageId. 
+    It is expected for the response body to simply be empty if there is no such message. The response status should always be 200, which is the default. */
+    @GetMapping("messages/{messageId}")
+    public ResponseEntity<Message> getMessageById(@PathVariable int messageId){
+        Message returnedMessage = messageService.getMessageById(messageId);
+        return ResponseEntity.status(200).body(returnedMessage);
+    }
+
+    /*As a User, I should be able to submit a DELETE request on the endpoint DELETE localhost:8080/messages/{messageId}.
+    - The deletion of an existing message should remove an existing message from the database. If the message existed, 
+    the response body should contain the number of rows updated (1). The response status should be 200, which is the default.
+    - If the message did not exist, the response status should be 200, but the response body should be empty. 
+    This is because the DELETE verb is intended to be idempotent, ie, multiple calls to the DELETE endpoint should respond with the same type of response. */
+    @DeleteMapping("messages/{messageId}")
+    public ResponseEntity<Integer> deleteMessageById(@PathVariable int messageId){
+        Integer rowsUpdated = messageService.deleteMessageById(messageId);
+        return ResponseEntity.status(200).body(rowsUpdated);
+    }
+
+    /*As a user, I should be able to submit a PATCH request on the endpoint PATCH localhost:8080/messages/{messageId}. 
+    The request body should contain a new messageText values to replace the message identified by messageId. 
+    The request body can not be guaranteed to contain any other information.
+    - The update of a message should be successful if and only if the message id already exists and the new messageText is not blank and is not over 255 characters.
+     If the update is successful, the response body should contain the number of rows updated (1), and the response status should be 200, which is the default. 
+     The message existing on the database should have the updated messageText.
+    - If the update of the message is not successful for any reason, the response status should be 400. (Client error) */
+    @PatchMapping("messages/{messageId}")
+    public ResponseEntity<Integer> updateMessageById(@PathVariable int messageId, @RequestBody Message mText){
+        String messageText = mText.getMessageText();
+        //System.out.println("messageText content  " +messageText);
+        if(messageText.length() > 0){
+            Integer rowsUpdated = messageService.updateMessageById(messageId, messageText);
+            if(rowsUpdated != null)
+                return ResponseEntity.status(200).body(rowsUpdated);
+        }     
+        return ResponseEntity.status(400).body(null);
+    }
+
+    /*As a user, I should be able to submit a GET request on the endpoint GET localhost:8080/accounts/{accountId}/messages.
+    - The response body should contain a JSON representation of a list containing all messages posted by a particular user,
+     which is retrieved from the database. It is expected for the list to simply be empty if there are no messages. 
+     The response status should always be 200, which is the default. */
+     @GetMapping("accounts/{accountId}/messages")
+     public ResponseEntity<List<Message>> getAllMessagesByUserId(@PathVariable int accountId){
+        List<Message> allMessages = messageService.getAllMessagesByUser(accountId);
+        return ResponseEntity.status(200).body(allMessages);
     }
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,5 +1,9 @@
 package com.example.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Message;
@@ -24,4 +28,65 @@ public class MessageService {
         }
         return null;
     }
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageById(int id){
+        Optional<Message> optional = messageRepository.findById(id) ;
+        if(optional.isPresent())
+            return optional.get();
+        //Message returnedMessage = 
+       
+        return null;
+    }
+
+    public Integer deleteMessageById(int id){
+        List<Message> messageList = messageRepository.findAll();
+        if(messageList.contains(getMessageById(id))){
+            int countBeforeDelete = messageRepository.findAll().size();
+
+            messageRepository.deleteById(id);
+
+            int countAfterDelete = messageRepository.findAll().size();
+            int rowsUpdated = countBeforeDelete - countAfterDelete;
+            System.out.println("method-deleteMessageById()  updated-rows   " + rowsUpdated);
+            if(rowsUpdated > 0)
+                return rowsUpdated;
+        }
+        
+        
+        return null;
+    }
+
+    public Integer updateMessageById(int id, String text){
+        List<Message> messageList = messageRepository.findAll();
+        Message messageTobeUpdated = getMessageById(id);
+        if(text.equals(""))
+            return null;
+        if(messageList.contains(messageTobeUpdated) &&
+             text.length() < 256){
+            
+             System.out.println("method----messageTobeUpdated.getMessageText().equals(text)---" + messageTobeUpdated.getMessageText() + "  " + text);
+            if(messageTobeUpdated.getMessageText() != text){
+                messageTobeUpdated.setMessageText(text);
+                messageRepository.save(messageTobeUpdated);
+                return 1;
+            }
+        }
+               
+        return null;
+    }
+
+    public List<Message> getAllMessagesByUser(int id){
+        List<Message> userMessages = new ArrayList<>();
+        List<Message> allMessages = messageRepository.findAll();
+        for(Message m: allMessages){
+            if(m.getPostedBy() == id)
+                userMessages.add(m);
+        }
+
+        return userMessages;
+    }
+
 }
